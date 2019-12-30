cf76b93f29e5331d925884a12954a42ea70e41c4
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-30 21:27:05 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,16 @@
 class Appointment
- 
+  attr_accessor :doctor, :patient, :date
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,29 @@
 require 'pry'
 class Doctor
+  attr_accessor :name
  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.each do|a|
+      a.doctor == self
+    end
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.collect{|a|a.patient}
+  end
 end
